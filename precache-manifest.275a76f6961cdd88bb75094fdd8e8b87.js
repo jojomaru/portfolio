e8806968f11c1a59be63f61acdf99fa6b5e1de16
jojomaru <kachinkmanworld@gmail.com>
2022-02-27 20:38:55 +0800

    Updates

---
+++ precache-manifest.275a76f6961cdd88bb75094fdd8e8b87.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b9ce70f47a68a2e60225",
+    "url": "/portfolio/static/css/main.936f0cc5.chunk.css"
+  },
+  {
+    "revision": "b9ce70f47a68a2e60225",
+    "url": "/portfolio/static/js/main.b9ce70f4.chunk.js"
+  },
+  {
+    "revision": "e9cf827b1d893b2cc3d9",
+    "url": "/portfolio/static/css/1.6ec8c5a8.chunk.css"
+  },
+  {
+    "revision": "e9cf827b1d893b2cc3d9",
+    "url": "/portfolio/static/js/1.e9cf827b.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "11f097ec0082b870015feb1973bedbab",
+    "url": "/portfolio/static/media/gps1.11f097ec.png"
+  },
+  {
+    "revision": "4b59b9d6a47439efc2b720893ebb4770",
+    "url": "/portfolio/static/media/gps2.4b59b9d6.png"
+  },
+  {
+    "revision": "c7adabd24b92cf2bea7b18897b7db7c2",
+    "url": "/portfolio/static/media/fyp.c7adabd2.png"
+  },
+  {
+    "revision": "05a1294c8d842c7d3446e9e32b4b1c00",
+    "url": "/portfolio/index.html"
+  }
+];
